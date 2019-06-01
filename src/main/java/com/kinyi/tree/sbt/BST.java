94c725f01e826d2f97cac241ccb9a80cc6228d74
kinyi <kinyi@hudongpai.com>
2019-06-01 09:54:44 +0800

    BST

---
+++ src/main/java/com/kinyi/tree/sbt/BST.java
@@ -0,0 +1,415 @@
+package com.kinyi.tree.sbt;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * @author Kinyi_Chan
+ * @since 2019-05-28
+ */
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        private E e;
+        private Node left;
+        private Node right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+//    /**
+//     * 向二分搜索树中添加新的元素e
+//     *
+//     * @param e
+//     */
+//    public void add(E e) {
+//        if (root == null) {
+//            root = new Node(e);
+//            size++;
+//        } else {
+//            add(root, e);
+//        }
+//    }
+//
+//    /**
+//     * 向以node为根的二分搜索树中插入元素e，递归算法
+//     * @param node
+//     * @param e
+//     */
+//    public void add(Node node, E e) {
+//        if (e.equals(node.e)) {
+//            return;
+//        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+//            node.left = new Node(e);
+//            size++;
+//            return;
+//        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+//            node.right = new Node(e);
+//            size++;
+//            return;
+//        }
+//        if (e.compareTo(node.e) < 0) {
+//            add(node.left, e);
+//        } else {
+//            add(node.right, e);
+//        }
+//    }
+
+    /**
+     * 向二分搜索树中添加新的元素e
+     *
+     * @param e
+     */
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    /**
+     * 向以node为根的二分搜索树中插入元素e，递归算法
+     * 返回插入新节点后二分搜索树的根
+     *
+     * @param node
+     * @param e
+     * @return
+     */
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+        return node;
+    }
+
+    /**
+     * 看二分搜索树中是否包含元素e
+     *
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    /**
+     * 看以node为根的二分搜索树中是否包含元素e，递归算法
+     *
+     * @param node
+     * @param e
+     * @return
+     */
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+        if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            return contains(node.right, e);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 二分搜索树的前序遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    /**
+     * 前序遍历以node为根的二分搜索树，递归算法
+     *
+     * @param node
+     */
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    /**
+     * 二分搜索树的非递归前序遍历
+     */
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node pop = stack.pop();
+            System.out.println(pop.e);
+            if (pop.right != null) {
+                stack.push(pop.right);
+            }
+            if (pop.left != null) {
+                stack.push(pop.left);
+            }
+        }
+    }
+
+    /**
+     * 二分搜索树的中序遍历
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    /**
+     * 中序遍历以node为根的二分搜索树，递归算法
+     *
+     * @param node
+     */
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    /**
+     * 二分搜索树的后序遍历
+     */
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    /**
+     * 后序遍历以node为根的二分搜索树，递归算法
+     *
+     * @param node
+     */
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    /**
+     * 二分搜索树的层序遍历
+     */
+    public void levelOrder() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node node = queue.remove();
+            System.out.println(node.e);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    /**
+     * 寻找二分搜索树的最小元素
+     *
+     * @return
+     */
+    public E minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        return minimum(root).e;
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    /**
+     * 寻找二分搜索树的最大元素
+     *
+     * @return
+     */
+    public E maximum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        return maximum(root).e;
+    }
+
+    private Node maximum(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    /**
+     * 从二分搜索树中删除最小值所在节点, 返回最小值
+     *
+     * @return
+     */
+    public E removeMin() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        E minimum = minimum();
+        root = removeMin(root);
+        return minimum;
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node right = node.right;
+            node.right = null;
+            size--;
+            return right;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    /**
+     * 从二分搜索树中删除最大值所在节点, 返回最大值
+     *
+     * @return
+     */
+    public E removeMax() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        E maximum = maximum();
+        root = removeMax(root);
+        return maximum;
+    }
+
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node left = node.left;
+            node.left = null;
+            size--;
+            return left;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    public void remove(E e) {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty");
+        }
+        root = remove(root, e);
+    }
+
+    /**
+     * 删除掉以node为根的二分搜索树中值为e的节点，递归算法
+     * 返回删除节点后新的二分搜索树的根
+     *
+     * @param node
+     * @param e
+     * @return
+     */
+    private Node remove(Node node, E e) {
+        if (node == null) {
+            return null;
+        }
+        if (e.compareTo(node.e) == 0) {
+            if (node.left == null) {
+                Node right = node.right;
+                node.right = null;
+                size--;
+                return right;
+            } else if (node.right == null) {
+                Node left = node.left;
+                node.left = null;
+                size--;
+                return left;
+            }
+            Node newRoot = new Node(minimum(node.right).e);
+            newRoot.left = node.left;
+            newRoot.right = removeMin(node.right);
+            node.left = node.right = null;
+            return newRoot;
+        } else if (e.compareTo(node.e) < 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else {
+            node.right = remove(node.right, e);
+            return node;
+        }
+    }
+
+    //---------------------下面代码不是我写的-------------------
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    // 生成以node为根节点，深度为depth的描述二叉树的字符串
+    private void generateBSTString(Node node, int depth, StringBuilder res){
+
+        if(node == null){
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e +"\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0 ; i < depth ; i ++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 2, 6, 8, 4, 2};
+        for (int num : nums) {
+            bst.add(num);
+        }
+//        BST.preOrder();
+//        System.out.println();
+//        BST.preOrderNR();
+//        System.out.println();
+//        BST.inOrder();
+//        System.out.println();
+//        BST.postOrder();
+//        System.out.println();
+//        BST.levelOrder();
+        bst.remove(5);
+        bst.preOrder();
+//        System.out.println(bst);
+    }
+}
---
+++ src/main/java/com/kinyi/tree/sbt/leetcode/UniqueMorseCodeWords.java
@@ -0,0 +1,96 @@
+package com.kinyi.tree.sbt.leetcode;
+
+/**
+ * @author Kinyi_Chan
+ * @since 2019-05-28
+ */
+public class UniqueMorseCodeWords {
+
+    class SBT<E extends Comparable<E>> {
+
+        private class Node {
+            private E e;
+            private Node left;
+            private Node right;
+
+            public Node(E e) {
+                this.e = e;
+                left = null;
+                right = null;
+            }
+        }
+
+        private Node root;
+        private int size;
+
+        public int getSize() {
+            return size;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        /**
+         * 向二分搜索树中添加新的元素e
+         *
+         * @param e
+         */
+        public void add(E e) {
+            if (root == null) {
+                root = new Node(e);
+                size++;
+            } else {
+                add(root, e);
+            }
+        }
+
+        /**
+         * 向以node为根的二分搜索树中插入元素e，递归算法
+         *
+         * @param node
+         * @param e
+         */
+        public void add(Node node, E e) {
+            if (e.equals(node.e)) {
+                return;
+            } else if (e.compareTo(node.e) < 0 && node.left == null) {
+                node.left = new Node(e);
+                size++;
+                return;
+            } else if (e.compareTo(node.e) > 0 && node.right == null) {
+                node.right = new Node(e);
+                size++;
+                return;
+            }
+            if (e.compareTo(node.e) < 0) {
+                add(node.left, e);
+            } else {
+                add(node.right, e);
+            }
+        }
+    }
+
+
+    class Solution {
+        public int uniqueMorseRepresentations(String[] words) {
+            SBT<String> tree = new SBT<>();
+            String[] pass = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+            for (String word : words) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < word.length(); i++) {
+                    sb.append(pass[word.codePointAt(i) - 97]);
+                }
+                tree.add(sb.toString());
+            }
+            return tree.getSize();
+        }
+    }
+
+    public static void main(String[] args) {
+        String word = "kinyi";
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.codePointAt(i) - 97);
+        }
+    }
+}
