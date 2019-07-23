8f0013ebea2594c57a88fe86edaee7d926d35b9a
kinyi <kinyi@hudongpai.com>
2019-07-23 19:51:02 +0800

    优先队列

---
+++ src/main/java/com/kinyi/heap/PriorityQueue.java
@@ -0,0 +1,41 @@
+package com.kinyi.heap;
+
+import com.kinyi.queue.Queue;
+
+/**
+ * @author Kinyi_Chan
+ * @since 2019-07-23
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        this.maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+}
